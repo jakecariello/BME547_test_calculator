70134c8a37c3716c6ebea66458f1e4cf613510e9
Jake Cariello <jakecariello@gmail.com>
2020-08-24 13:22:38 -0400

    Add HDL analysis and reporting

--- calculator.py
+++ calculator.py
@@ -12,15 +12,25 @@ def interface():
 
 def HDL_driver():
     # get input
-    hdl = get_input()
+    hdl = get_input_hdl()
     # check if HDL is normal
     response = check_hdl(hdl)
     # output
-    output(response)
+    output_hdl(hdl, response)
 
-def get_input() -> int:
+def get_input_hdl() -> int:
     return int(input('Enter HDL test result: '))
 
+def check_hdl(value: int) -> str:
+    if value >= 60:
+        return 'Normal'
+    elif 40 <= value < 60:
+        return 'Borderline Low'
+    else:
+        return 'Low'
+
+def output_hdl(value: int, response: str):
+    print('HDL value of {} is classified as \'{}\''.format(value, response))
 
 interface()
 

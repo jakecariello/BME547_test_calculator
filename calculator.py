e730f50c6498d540b13bb96f0652e005012dc798
Jake Cariello <jakecariello@gmail.com>
2020-08-24 13:35:57 -0400

    Generify analysis and output functions, then add to LDL driver

--- calculator.py
+++ calculator.py
@@ -17,26 +17,39 @@ def HDL_driver():
     # get input
     hdl = get_input('HDL')
     # check if HDL is normal
-    response = check_hdl(hdl)
+    response = check('HDL', hdl)
     # output
-    output_hdl(hdl, response)
+    output('HDL', hdl, response)
 
 def LDL_driver():
     ldl = get_input('LDL')
+    response = check('LDL', ldl)
+    output('LDL', ldl, response)
 
 def get_input(input_type: str) -> int:
     return int(input('Enter {} test result: '.format(input_type)))
 
-def check_hdl(value: int) -> str:
-    if value >= 60:
-        return 'Normal'
-    elif 40 <= value < 60:
-        return 'Borderline Low'
-    else:
-        return 'Low'
+def check(check_type: str, value: int) -> str:
+    assert check_type in ['LDL', 'HDL'], 'Invalid check_type option!'
+    if check_type == 'HDL':
+        if value >= 60:
+            return 'Normal'
+        elif 40 <= value < 60:
+            return 'Borderline Low'
+        else:
+            return 'Low'
+    elif check_type == 'LDL':
+        if value >= 190:
+            return 'Very High'
+        elif value in range(160, 190):
+            return 'High'
+        elif value in range(130, 160):
+            return 'Borderline High'
+        else:
+            return 'Normal'
 
-def output_hdl(value: int, response: str):
-    print('HDL value of {} is classified as \'{}\''.format(value, response))
+def output(output_type: str, value: int, response: str):
+    print('{} value of {} is classified as \'{}\''.format(output_type, value, response))
 
 interface()
 
